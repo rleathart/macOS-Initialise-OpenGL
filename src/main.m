f662a8a17820c480b4363e299e6380ce273fd3f2
Robin Leathart <56587304+rleathart@users.noreply.github.com>
2022-09-29 17:15:27 +0100

    Add a modern shader version

--- src/main.m
+++ src/main.m
@@ -2,10 +2,20 @@
 
 #include <AppKit/AppKit.h>
 #include <OpenGL/gl.h>
+#include <dlfcn.h>
 
 NSOpenGLContext* GLContext;
 int Running = 1;
-int NeedsResize = 0;
+int NeedsResize = 1;
+
+GLint GlobalShaderProgram;
+
+// NOTE(robin): These two functions are not provided in the Apple OpenGL header
+// so we need to define them here and load them at runtime
+typedef void glBindVertexArrayFn(GLuint id);
+typedef void glGenVertexArraysFn(GLsizei n, GLuint *ids);
+glBindVertexArrayFn* glBindVertexArray;
+glGenVertexArraysFn* glGenVertexArrays;
 
 // NOTE(robin): We need a very simple delegate here to allow us
 // to access information about when the window is closed or resized.
@@ -29,6 +39,34 @@ void RenderThread(NSOpenGLContext* GLContext, NSWindow* Window)
 {
   [GLContext makeCurrentContext];
 
+  typedef struct
+  {
+    float x, y, z;
+    float r, g, b, a;
+  } vertex;
+
+  vertex Vertices[] =
+  {
+    // Position           Colour
+    { -.5f, -.5f, 0.0f,   1.0f, 0.0f, 0.0f, 1.0f },
+    { 0.5f, -.5f, 0.0f,   0.0f, 1.0f, 0.0f, 1.0f },
+    { 0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f, 1.0f },
+  };
+
+  GLuint VBO, VAO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, x));
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, r));
+
+  glUseProgram(GlobalShaderProgram);
+
   while (Running)
   {
     if (NeedsResize)
@@ -36,7 +74,6 @@ void RenderThread(NSOpenGLContext* GLContext, NSWindow* Window)
       NSRect Frame = [Window contentView].bounds;
       double Scale = [[Window screen] backingScaleFactor];
 
-      glLoadIdentity();
       glViewport(0, 0, Frame.size.width * Scale, Frame.size.height * Scale);
 
       // NOTE(robin): update has to be called on the main thread so we dispatch it here
@@ -46,16 +83,7 @@ void RenderThread(NSOpenGLContext* GLContext, NSWindow* Window)
       NeedsResize = 0;
     }
 
-    glClearColor(0, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-    {
-      glColor3f(1, 0, 0); glVertex2f(-0.5, -0.5);
-      glColor3f(0, 1, 0); glVertex2f(0, 0.5);
-      glColor3f(0, 0, 1); glVertex2f(0.5, -0.5);
-    }
-    glEnd();
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     [GLContext flushBuffer]; // NOTE(robin): Swap the backbuffer
   }
@@ -89,9 +117,9 @@ int main(void)
     // Otherwise choose a modern profile to get a context that is compatible with the
     // modern shader based pipeline.
     NSOpenGLPFAOpenGLProfile,
-    NSOpenGLProfileVersionLegacy,
+    // NSOpenGLProfileVersionLegacy,
     // NSOpenGLProfileVersion3_2Core,
-    // NSOpenGLProfileVersion4_1Core,
+    NSOpenGLProfileVersion4_1Core,
     0
   };
 
@@ -106,6 +134,72 @@ int main(void)
 
   printf("OpenGL Renderer: %s\n", glGetString(GL_RENDERER));
   printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+
+  GLint VertexShader = glCreateShader(GL_VERTEX_SHADER);
+  GLint FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  GlobalShaderProgram = glCreateProgram();
+
+  const char* VertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPosition;"
+    "layout (location = 1) in vec4 aColour;"
+    "out vec4 Colour;"
+    "void main(void)"
+    "{"
+    "  gl_Position = vec4(aPosition, 1.0);"
+    "  Colour = aColour;"
+    "}"
+    ;
+
+  const char* FragmentShaderSource =
+    "#version 330 core\n"
+    "in vec4 Colour;"
+    "out vec4 Fragment;"
+    "void main(void)"
+    "{"
+    "  Fragment = Colour;"
+    "}"
+    ;
+
+  glShaderSource(VertexShader, 1, &VertexShaderSource, 0);
+  glShaderSource(FragmentShader, 1, &FragmentShaderSource, 0);
+
+  glCompileShader(VertexShader);
+  glCompileShader(FragmentShader);
+
+  char Log[512] = {0};
+
+  glGetShaderInfoLog(VertexShader, sizeof(Log), 0, Log); printf("%s", Log);
+  glGetShaderInfoLog(FragmentShader, sizeof(Log), 0, Log); printf("%s", Log);
+
+  glAttachShader(GlobalShaderProgram, VertexShader);
+  glAttachShader(GlobalShaderProgram, FragmentShader);
+
+  glLinkProgram(GlobalShaderProgram);
+  glGetProgramInfoLog(GlobalShaderProgram, sizeof(Log), 0, Log); printf("%s", Log);
+
+  // ==========Load OpenGL Extension Pointers==========
+
+  const char* PossiblePaths[] = {
+    "../Frameworks/OpenGL.framework/OpenGL",
+    "/Library/Frameworks/OpenGL.framework/OpenGL",
+    "/System/Library/Frameworks/OpenGL.framework/OpenGL",
+    "/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL"
+  };
+
+  void* LibGL = 0;
+
+  for (int i = 0; i < sizeof(PossiblePaths) / sizeof(PossiblePaths[0]); i++)
+  {
+    LibGL = dlopen(PossiblePaths[i], RTLD_LAZY | RTLD_LOCAL);
+    if (LibGL)
+      break;
+  }
+
+  glGenVertexArrays = dlsym(LibGL, "glGenVertexArrays");
+  glBindVertexArray = dlsym(LibGL, "glBindVertexArray");
+
+  // ==================================================
 
   // NOTE(robin): On macOS only the main thread is permitted to update the UI and
   // receive events. Because of this, we reserve the main thread for event processing
